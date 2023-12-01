f17d36e03d9cd3532a6db3385d63218a599cf08a
Rafael Carreño <rafacarreto@hotmail.com>
2023-11-30 21:17:26 -0500

    Ajustes funcionando con administrador

--- cf7-redirection.php
+++ cf7-redirection.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom CF7 Redirect
 Description: Redirige determinados formularios de Contact Form 7 después de enviarlos.
 Version: 1.0
-Author: Rafael Carreño Olaya
+Author: Tu Nombre
 */
 
 // Acciones cuando se activa y desactiva el plugin
@@ -31,13 +31,15 @@ function custom_cf7_redirect()
     // Obtiene la lista de IDs de formularios almacenados
     $form_ids = get_option('custom_cf7_redirect_form_ids', array());
 
-    // ID del formulario de Contact Form 7 que quieres redirigir
-    $form_id_to_redirect = 123;
+    // Obtén el ID del formulario actual
+    $current_form_id = intval($_POST['_wpcf7']);
 
-    // Verifica si el formulario actual coincide con el ID que quieres redirigir
-    if (in_array($form_id_to_redirect, $form_ids)) {
+    var_dump($current_form_id);
+
+    // Verifica si el formulario actual coincide con algún ID que quieres redirigir
+    if (in_array($current_form_id, $form_ids)) {
         // URL a la que quieres redirigir después de enviar el formulario
-        $redirect_url = 'https://tu-sitio.com/pagina-de-destino';
+        $redirect_url = '/inicio';
 
         // Redirige al usuario
         wp_redirect($redirect_url);
@@ -59,75 +61,68 @@ function custom_cf7_redirect_admin_menu()
 
 function custom_cf7_redirect_settings_page()
 {
+    // Obtiene los IDs de formularios almacenados
+    $form_ids = get_option('custom_cf7_redirect_form_ids', array());
+
+    // Procesar formularios enviados
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Actualizar IDs con los valores del formulario
+        $form_ids = isset($_POST['custom_cf7_redirect_form_ids']) ? array_map('sanitize_text_field', explode(',', $_POST['custom_cf7_redirect_form_ids'])) : array();
+
+        // Agregar 0 por defecto si no se ha ingresado ningún ID
+        if (empty($form_ids)) {
+            $form_ids = array(0);
+        }
+
+        // Guardar los IDs actualizados
+        update_option('custom_cf7_redirect_form_ids', $form_ids);
+
+        // Mostrar mensaje de actualización
+        echo '<div class="updated"><p>Configuración actualizada correctamente.</p></div>';
+    }
+
     // Muestra el formulario de configuración
     ?>
     <div class="wrap">
         <h2>Configuración de Redirección CF7</h2>
-        <form method="post" action="options.php">
+        <form method="post" action="">
             <?php
-            // Carga las opciones guardadas
-            settings_fields('custom_cf7_redirect_options');
-            do_settings_sections('custom-cf7-redirect-settings');
-            submit_button();
+            // Agrega un campo nonce para seguridad
+            wp_nonce_field('custom_cf7_redirect_settings', 'custom_cf7_redirect_nonce');
+            ?>
+            <label for="custom_cf7_redirect_form_ids">IDs de Formularios a Redirigir (separados por comas):</label>
+            <input type="text" name="custom_cf7_redirect_form_ids" id="custom_cf7_redirect_form_ids" value="<?php echo esc_attr(implode(',', $form_ids)); ?>" />
+            <?php
+            submit_button('Guardar Cambios');
             ?>
         </form>
+
+        <!-- Mostrar los IDs guardados -->
+        <h3>IDs Guardados:</h3>
+        <?php
+        if (!empty($form_ids)) {
+            echo '<ul>';
+            foreach ($form_ids as $id) {
+                echo '<li>' . esc_html($id) . '</li>';
+            }
+            echo '</ul>';
+        } else {
+            echo '<p>No se han guardado IDs.</p>';
+        }
+        ?>
     </div>
     <?php
 }
+
+
 
 // Acciones para inicializar y registrar las opciones del plugin
 add_action('admin_init', 'custom_cf7_redirect_admin_init');
 
 function custom_cf7_redirect_admin_init()
 {
-    // Registra una sección y campos de opciones
-    add_settings_section(
-        'custom_cf7_redirect_section',
-        'Configuración de IDs de Formularios',
-        'custom_cf7_redirect_section_callback',
-        'custom-cf7-redirect-settings'
-    );
-
-    add_settings_field(
-        'custom_cf7_redirect_form_ids',
-        'IDs de Formularios a Redirigir',
-        'custom_cf7_redirect_form_ids_callback',
-        'custom-cf7-redirect-settings',
-        'custom_cf7_redirect_section'
-    );
-
-    // Registra las opciones con una función de validación personalizada
-    register_setting('custom_cf7_redirect_options', 'custom_cf7_redirect_form_ids', 'custom_cf7_redirect_validate');
+    // Registra una sección y campos de opciones (opcional, ya que no estás usando configuraciones de registro)
 }
 
-function custom_cf7_redirect_section_callback()
-{
-    // Puedes agregar instrucciones o descripciones aquí
-}
 
-function custom_cf7_redirect_form_ids_callback()
-{
-    // Obtiene los IDs de formularios almacenados
-    $form_ids = get_option('custom_cf7_redirect_form_ids', array());
-
-    // Muestra un campo de texto para cada ID de formulario
-    echo '<input type="text" name="custom_cf7_redirect_form_ids[]" value="' . esc_attr(implode(',', $form_ids)) . '" />';
-}
-
-function custom_cf7_redirect_validate($input)
-{
-    // Validación: Asegúrate de que cada ID sea un número y está separado por comas
-    $validated_ids = array_map('intval', explode(',', $input));
-
-    // Elimina valores duplicados
-    $validated_ids = array_unique($validated_ids);
-
-    // Filtra los valores no válidos
-    $validated_ids = array_filter($validated_ids, 'is_numeric');
-
-    // Puedes agregar más validaciones según sea necesario
-
-    // Retorna los IDs validados
-    return $validated_ids;
-}
 ?>
